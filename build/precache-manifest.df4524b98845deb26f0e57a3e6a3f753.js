4b990e346c0d11416b7d50e10d8972d63881495f
williamslsy <donwaleyb@gmail.com>
2022-03-28 22:32:40 +0100

    new changes

---
+++ build/precache-manifest.df4524b98845deb26f0e57a3e6a3f753.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf4a792307d5036f11632d675e4eea7",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "769df42928a864891222",
+    "url": "/netflix/static/css/2.61061fbe.chunk.css"
+  },
+  {
+    "revision": "9693ddcf70b058bd26c9",
+    "url": "/netflix/static/css/main.b9691cad.chunk.css"
+  },
+  {
+    "revision": "769df42928a864891222",
+    "url": "/netflix/static/js/2.f74ddf94.chunk.js"
+  },
+  {
+    "revision": "e14686b76b1e94ce0fb7013108901e08",
+    "url": "/netflix/static/js/2.f74ddf94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9693ddcf70b058bd26c9",
+    "url": "/netflix/static/js/main.61bf084d.chunk.js"
+  },
+  {
+    "revision": "05c693931a947309def3",
+    "url": "/netflix/static/js/runtime-main.218945ab.js"
+  },
+  {
+    "revision": "9dd160927594f5e2f081a6922df2ed1d",
+    "url": "/netflix/static/media/aboutbg.9dd16092.jpg"
+  },
+  {
+    "revision": "1e45f5316d660cf47ec2bb672076688d",
+    "url": "/netflix/static/media/default.1e45f531.jpg"
+  },
+  {
+    "revision": "43eb6bf328631a6b572bd71c22d04d68",
+    "url": "/netflix/static/media/logo.43eb6bf3.png"
+  },
+  {
+    "revision": "d2edb29f3f970c36aadecbb01ed0bb79",
+    "url": "/netflix/static/media/nfbg.d2edb29f.jpg"
+  },
+  {
+    "revision": "1f8752ba60ad4a50b0502d21a2a8765b",
+    "url": "/netflix/static/media/tmdb.1f8752ba.svg"
+  }
+]);
